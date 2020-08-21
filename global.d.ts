e206e2139abea89efe81c6970814afd14697a24d
Shigma <1700011071@pku.edu.cn>
2020-08-22 04:01:26 +0800

    init eat

---
+++ global.d.ts
@@ -0,0 +1,15 @@
+declare module 'koishi' {
+  interface User {
+    name: string
+  }
+
+  interface Argv {
+    user: User
+    options: Record<string, any>
+    args: string[]
+    send(...args: any[]): Promise<void>
+    exec(message: string): Promise<void>
+  }
+
+  export function registerCommand(name: string, callback: (argv: Argv) => void | string | Promise<void | string>): void
+}
