08450753fbae11f3b9a62ddae152336307e5f203
Shigma <1700011071@pku.edu.cn>
2020-08-25 23:29:12 +0800

    lint autofix

--- eat/index.ts
+++ eat/index.ts
@@ -12,10 +12,10 @@ registerCommand('eat', ({ args, user }) => {
   } else if (name.length > 50) {
     return '这什么鬼东西啊，名字那么长，总感觉有毒，要不你先吃一个?我6个小时后来看看你是否还活着。。。'
   } else {
-    let sa = specialAnswers.find((e) => {
+    const sa = specialAnswers.find((e) => {
       return e.name === name
     })
-    let answer = getAnswer(sa ? sa.answers : answers, user)
+    const answer = getAnswer(sa ? sa.answers : answers, user)
     if (Array.isArray(answer)) {
       return answer.join('\n')
     } else {
@@ -35,16 +35,14 @@ function getAnswer(answers: Answer[], user: User) {
     pointer += (answer.probability || 1)
     if (target < pointer) {
       switch (typeof answer.text) {
-        case "function":
+        case 'function':
           return answer.text(user)
-          break;
-        case "string":
+        case 'string':
           return answer.text.replace(/\%user\%/g, user.name).replace(/\%name\%/g, name)
         default:
           return answer.text.map((e) => {
             return e.replace(/\%user\%/g, user.name).replace(/\%name\%/g, name)
           })
-          break;
       }
     }
   }
