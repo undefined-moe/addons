24a041befe5fce98dccefeea997c8cd374f500be
Shugen <shugen002@users.noreply.github.com>
2020-08-26 00:16:44 +0800

    fix

--- eat/index.ts
+++ eat/index.ts
@@ -4,18 +4,18 @@ import specialAnswers from './SpecialAnswer'
 import { Answer } from './types'
 
 registerCommand('eat', ({ args, user }) => {
-  const name = args.join(' ')
-  if (!name) {
+  const item = args.join(' ')
+  if (!item) {
     return '请输入要喂食的物品名。'
-  } else if (name.match(/\[cq:at,qq=\d+\]/ig)) {
+  } else if (item.match(/\[cq:at,qq=\d+\]/ig)) {
     return '仁义道德'.repeat(Math.floor(Math.random() * 15 + 1))
-  } else if (name.length > 50) {
+  } else if (item.length > 50) {
     return '这什么鬼东西啊，名字那么长，总感觉有毒，要不你先吃一个?我6个小时后来看看你是否还活着。。。'
   } else {
     const sa = specialAnswers.find((e) => {
-      return e.name === name
+      return e.name === item
     })
-    const answer = getAnswer(sa ? sa.answers : answers, user)
+    const answer = getAnswer(sa ? sa.answers : answers, item, user)
     if (Array.isArray(answer)) {
       return answer.join('\n')
     } else {
@@ -24,7 +24,7 @@ registerCommand('eat', ({ args, user }) => {
   }
 })
 
-function getAnswer(answers: Answer[], user: User) {
+function getAnswer(answers: Answer[], item: string, user: User) {
   var total = 0
   answers.forEach((e) => {
     total += (e.probability || 1)
@@ -38,10 +38,10 @@ function getAnswer(answers: Answer[], user: User) {
         case 'function':
           return answer.text(user)
         case 'string':
-          return answer.text.replace(/\%user\%/g, user.name).replace(/\%name\%/g, name)
+          return answer.text.replace(/\%user\%/g, user.name).replace(/\%name\%/g, item)
         default:
           return answer.text.map((e) => {
-            return e.replace(/\%user\%/g, user.name).replace(/\%name\%/g, name)
+            return e.replace(/\%user\%/g, user.name).replace(/\%name\%/g, item)
           })
       }
     }
