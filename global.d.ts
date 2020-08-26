3a426a2a91c5b45c0fa2fa4cde7c9432e1aa8c56
Shigma <1700011071@pku.edu.cn>
2020-08-26 12:24:16 +0800

    shiki api

--- global.d.ts
+++ global.d.ts
@@ -20,3 +20,39 @@ declare module 'koishi/utils' {
 
   export { Time, Random, CQCode }
 }
+
+declare module 'shiki' {
+  export enum rarities { N, R, SR, SSR, EX, SP }
+  export type Rarity = keyof typeof rarities
+
+  export interface Item {
+    name: string
+    rarity: Rarity
+    description: string
+    maxCount?: number
+    value?: number
+    bid?: number
+    onGain?: Event
+    onLose?: Event
+    lottery?: number
+    fishing?: number
+    plot?: boolean
+  }
+
+  type NoticeName =
+    | 'herbs' | 'assistant' | 'pagoda' | 'concert'
+    | 'blood' | 'hurricane' | 'thundercloud' | 'emergency'
+    | 'challenge' | 'toad' | 'kadama' | 'catfish'
+    | 'spa' | 'dango' | 'moriya'
+
+  export interface NoticeData {
+    id: NoticeName
+    start: number
+    args: string[]
+  }
+
+  export const items: Record<string, Item> & Item[] & Record<Rarity, Item[]>
+  export const probs: Record<Rarity, number>
+  export const accurate: Record<'N' | 'R' | 'SR' | 'SSR', string[]>
+  export const notices: NoticeData[]
+}
