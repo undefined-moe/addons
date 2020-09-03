533354a20e30695faee588c0c19b66a2fe3f29b8
undefined <masnn0@outlook.com>
2020-09-03 13:17:49 +0800

    2

--- o/index.ts
+++ o/index.ts
@@ -1,5 +1,5 @@
 import { registerCommand } from 'koishi/addons'
 
 registerCommand('o', ({ args }) => {
-    return args[0].replace(/o/gmi, '[CQ:face,id=107]');
+    return args.join(' ').replace(/o/gmi, '[CQ:face,id=107]');
 })
